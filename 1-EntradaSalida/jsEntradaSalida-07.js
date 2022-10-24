dbd96b5ba677b51b08017330d7e84abc4c5ef3c7
Christian Baus <christianbaus@hotmail.com>
2022-10-24 10:37:43 -0300

    primer commit

---
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -0,0 +1,20 @@
+/*
+Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/
+function sumar() {
+  alert("sumar");
+}
+
+function restar() {
+  alert("restar");
+}
+
+function multiplicar() {
+  alert("multiplicar");
+}
+
+function dividir() {
+  alert("dividir");
+}
